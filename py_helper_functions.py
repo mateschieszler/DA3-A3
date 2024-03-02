fb33bab26df5f9c5680620c9abff8f78928929d2
Peter Szilvasi <szilvasi_peter@student.ceu.edu>
2024-03-02 20:01:55 +0100

    pushed final codes, still without comments

--- py_helper_functions.py
+++ py_helper_functions.py
@@ -467,3 +467,97 @@ def add_margin(ax, x=0.05, y=0.05) -> None:
 
     ax.set_xlim(xlim[0] - xmargin, xlim[1] + xmargin)
     ax.set_ylim(ylim[0] - ymargin, ylim[1] + ymargin)
+    
+# Péter
+def regression_results(y_true, y_pred):
+
+    # Regression metrics
+    explained_variance=metrics.explained_variance_score(y_true, y_pred)
+    mean_absolute_error=metrics.mean_absolute_error(y_true, y_pred) 
+    mse=metrics.mean_squared_error(y_true, y_pred) 
+    median_absolute_error=metrics.median_absolute_error(y_true, y_pred)
+    r2=metrics.r2_score(y_true, y_pred)
+
+    print('explained_variance: ', round(explained_variance,4))    
+    print('r2: ', round(r2,4))
+    print('MAE: ', round(mean_absolute_error,4))
+    print('MSE: ', round(mse,4))
+    print('RMSE: ', round(np.sqrt(mse),4))
+    
+def create_coef_matrix(X, model):
+    coef_matrix = pd.concat(
+        [pd.DataFrame(X.columns),pd.DataFrame(model.coef_.flatten())], axis = 1
+    )
+    coef_matrix.columns = ['variable', 'coefficient']
+    coef_matrix.iloc[-1] = ['Intercept', model.intercept_.flatten()[0]]
+    return coef_matrix
+
+def cv_summary(lambdas, C_values, model):
+    d = {'lambdas': lambdas, 'C_values': C_values, 'mean_cv_score': model.scores_[1].mean(axis = 0)}
+    return(pd.DataFrame(data=d))
+
+def create_roc_plot(y_true, y_pred):
+    fpr, tpr, thresholds = roc_curve(y_true, y_pred)
+    all_coords = pd.DataFrame({
+        'fpr': fpr,
+        'tpr': tpr,
+        'thresholds': thresholds
+    })
+    
+    plot = ggplot(all_coords, aes(x = 'fpr', y = 'tpr')) \
+        + geom_line(color=color[0], size = 0.7) \
+        + geom_area(position = 'identity', fill = 'mediumaquamarine', alpha = 0.3) \
+        + xlab("False Positive Rate (1-Specifity)") \
+        + ylab("True Positive Rate (Sensitivity)") \
+        + geom_abline(intercept = 0, slope = 1,  linetype = "dotted", color = "black") \
+        + scale_y_continuous(limits = (0, 1), breaks = seq(0, 1, .1), expand = (0, 0.01)) \
+        + scale_x_continuous(limits = (0, 1), breaks = seq(0, 1, .1), expand = (0.01, 0)) \
+        + theme_bw()
+    return(plot)
+
+def sigmoid_array(x):
+    return(1 / (1 + np.exp(-x)))
+
+def generate_fold_prediction(model, X, fold, param_index):
+    fold_coef = model.coefs_paths_[1][fold,param_index,:]
+    return(sigmoid_array(np.dot(X, np.transpose(fold_coef)[:-1]) +  np.transpose(fold_coef)[-1]))
+
+def create_loss_plot(all_coords, optimal_threshold, curr_exp_loss):
+    all_coords_copy = all_coords.copy()
+    all_coords_copy['loss'] = (all_coords_copy.false_pos*FP + all_coords_copy.false_neg*FN)/all_coords_copy.n
+    
+    t = optimal_threshold
+    l = curr_exp_loss
+    
+    plot = ggplot(all_coords_copy, aes(x = 'thresholds', y = 'loss')) + \
+        geom_line(color=color[0], size=0.7) + \
+        scale_x_continuous(breaks = seq(0, 1.1, by = 0.1)) + \
+        coord_cartesian(xlim=(0,1))+ \
+        geom_vline(xintercept = t , color = color[0] ) + \
+        annotate(geom = "text", x = t - 0.01, y= max(all_coords_copy.loss) - 0.4,
+                 label="best threshold: " + str(round(t,2)),
+                 colour=color[1], angle=90, size = 7) +\
+        annotate(geom = "text", x = t + 0.06, y= l,\
+                 label= str(round(l, 2)), size = 7) +\
+        theme_bw()
+    return(plot)
+
+
+def create_roc_plot_with_optimal(all_coords, optimal_threshold):
+    all_coords_copy = all_coords.copy()
+    all_coords_copy['sp'] = all_coords_copy.true_neg/all_coords_copy.neg
+    all_coords_copy['se'] = all_coords_copy.true_pos/all_coords_copy.pos
+    
+    best_coords = all_coords_copy[all_coords_copy.thresholds == optimal_threshold]
+    sp = best_coords.sp.values[0]
+    se = best_coords.se.values[0]
+
+    plot = ggplot(all_coords_copy, aes(x = 'sp', y = 'se')) +\
+        geom_line(color=color[0], size=0.7) +\
+        scale_y_continuous(breaks = seq(0, 1.1, by = 0.1)) +\
+        scale_x_reverse(breaks = seq(0, 1.1, by = 0.1)) +\
+        geom_point(data = pd.DataFrame({'sp': [sp], 'se': [se]})) +\
+        annotate(geom = "text", x = sp, y = se + 0.03,
+                 label = str(round(sp, 2)) + ', ' + str(round(se, 2)), size = 7) +\
+        theme_bw()
+    return(plot)
